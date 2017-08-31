3089b8f261af1c5682d4482df6d9f8a5456f8add
Adrian Esteban Madrid <aemadrid@gmail.com>
2017-08-31 17:19:22 -0600

    Replaces react with preact and preact-compat.

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -3,31 +3,31 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
-const webpack = require('webpack')
+const webpack                                        = require('webpack')
 const { basename, dirname, join, relative, resolve } = require('path')
-const { sync } = require('glob')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const ManifestPlugin = require('webpack-manifest-plugin')
-const extname = require('path-complete-extname')
-const { env, settings, output, loadersDir } = require('./configuration.js')
+const { sync }                                       = require('glob')
+const ExtractTextPlugin                              = require('extract-text-webpack-plugin')
+const ManifestPlugin                                 = require('webpack-manifest-plugin')
+const extname                                        = require('path-complete-extname')
+const { env, settings, output, loadersDir }          = require('./configuration.js')
 
 const extensionGlob = `**/*{${settings.extensions.join(',')}}*`
-const entryPath = join(settings.source_path, settings.source_entry_path)
-const packPaths = sync(join(entryPath, extensionGlob))
+const entryPath     = join(settings.source_path, settings.source_entry_path)
+const packPaths     = sync(join(entryPath, extensionGlob))
 
 module.exports = {
   entry: packPaths.reduce(
     (map, entry) => {
-      const localMap = map
-      const namespace = relative(join(entryPath), dirname(entry))
+      const localMap                                             = map
+      const namespace                                            = relative(join(entryPath), dirname(entry))
       localMap[join(namespace, basename(entry, extname(entry)))] = resolve(entry)
       return localMap
     }, {}
   ),
 
   output: {
-    filename: '[name].js',
-    path: output.path,
+    filename:   '[name].js',
+    path:       output.path,
     publicPath: output.publicPath
   },
 
@@ -39,17 +39,21 @@ module.exports = {
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
     new ManifestPlugin({
-      publicPath: output.publicPath,
+      publicPath:      output.publicPath,
       writeToFileEmit: true
     })
   ],
 
   resolve: {
     extensions: settings.extensions,
-    modules: [
+    modules:    [
       resolve(settings.source_path),
       'node_modules'
-    ]
+    ],
+    alias:      {
+      'react':     'preact-compat',
+      'react-dom': 'preact-compat'
+    }
   },
 
   resolveLoader: {
